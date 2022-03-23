2f91a1e956cb60d7ae01b899e6efb30701c41eb5
KipoDipo <kikodimov2002@gmail.com>
2022-03-23 18:28:25 +0200

    actually nvm

--- planeshw/Plane.h
+++ planeshw/Plane.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <iostream>
 
 class Plane
 {
@@ -9,24 +8,10 @@ private:
 	char FirstName[256];
 	char LastName[256];
 public:
-	Plane()
-	{
-	}
-	Plane(const long long int ID, const char* Name, const char* Type, const long long int Flights)
-	{
-		this->ID = ID;
-		strcpy_s(FirstName, Name);
-		strcpy_s(LastName, Type);
-		this->Flights = Flights;
-	}
-	long long int GetID() const
-	{
-		return ID;
-	}
-	void Print() const
-	{
-		std::cout << ID << " " << FirstName << " " << LastName << " " << Flights << std::endl;
-	}
+	Plane();
+	Plane(const long long int ID, const char* Name, const char* Type, const long long int Flights);
+	long long int GetID() const;
+	void Print() const;
 };
 
 Plane CreatePlaneFromInput()
--- planeshw/Source.cpp
+++ planeshw/Source.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 #include <fstream>
-#include "Plane.h"
 #define FILE_PATH "Planes.db"
 
 using namespace std;
+
+class Plane
+{
+private:
+	long long int Flights;
+	long long int ID;
+	char FirstName[256];
+	char LastName[256];
+public:
+	Plane()
+	{
+	}
+	Plane(const long long int ID, const char* Name, const char* Type, const long long int Flights)
+	{
+		this->ID = ID;
+		strcpy_s(FirstName, Name);
+		strcpy_s(LastName, Type);
+		this->Flights = Flights;
+	}
+	long long int GetID() const
+	{
+		return ID;
+	}
+	void Print() const
+	{
+		std::cout << ID << " " << FirstName << " " << LastName << " " << Flights << std::endl;
+	}
+};
+
+Plane CreatePlaneFromInput()
+{
+	long long int id;
+	char name[256];
+	char type[256];
+	long long int flights;
+	std::cin >> id >> name >> type >> flights;
+	return Plane(id, name, type, flights);
+}
 
 int GetElementCountFromFile(ifstream& stream, int sizeOfClass)
 {
