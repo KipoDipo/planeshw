19b8730abf937e05f948773664bff3887dd03ee7
KipoDipo <kikodimov2002@gmail.com>
2022-03-23 07:59:59 +0200

    Add project files.

---
+++ planeshw/Source.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#define FILE_PATH "Planes.db"
+
+using namespace std;
+
+class Plane
+{
+private:
+	long long int Flights;
+	long long int ID;
+	char FirstName[256];
+	char LastName[256];
+public:
+	Plane()
+	{
+	}
+	Plane(long long int ID, char* Name, char* Type, long long int Flights)
+	{
+		this->ID = ID;
+		strcpy_s(FirstName, Name);
+		strcpy_s(LastName, Type);
+		this->Flights = Flights;
+	}
+	long long int GetID()
+	{
+		return ID;
+	}
+	void Print()
+	{
+		cout << ID << " " << FirstName << " " << LastName << " " << Flights << endl;
+	}
+};
+
+Plane CreatePlane()
+{
+	long long int id;
+	char name[256];
+	char type[256];
+	long long int flights;
+	cin >> id >> name >> type >> flights;
+	return Plane(id, name, type, flights);
+}
+
+int GetElementCountFromFile(ifstream& stream, int sizeOfClass)
+{
+	stream.seekg(0, stream.end);
+	int length = stream.tellg() / sizeOfClass;
+	stream.seekg(0, stream.beg);
+	return length;
+}
+
+int main()
+{
+	ofstream outFile(FILE_PATH, ios::binary | ios::app);
+
+	if (!outFile.is_open())
+	{
+		cout << "err...";
+		return -1;
+	}
+
+	char command[32];
+	while (strcmp(command, "exit") != 0)
+	{
+		cin >> command;
+		if (strcmp(command, "create") == 0)
+		{
+			Plane newPlane = CreatePlane();
+			outFile.write(reinterpret_cast<const char*>(&newPlane), sizeof(newPlane));
+			cout << "Record saved!\n";
+		}
+		else if (strcmp(command, "search") == 0)
+		{
+			long long int id;
+			cin >> id;
+			bool hasFoundId = false;
+
+			ifstream inFile(FILE_PATH, ios::binary);
+
+			int planesInFile = GetElementCountFromFile(inFile, sizeof(Plane));
+			Plane* planes = new Plane[planesInFile];
+
+			for (int i = 0; i < planesInFile; i++)
+			{
+				inFile.read(reinterpret_cast<char*>(&planes[i]), sizeof(Plane));
+				if (planes[i].GetID() == id)
+				{
+					planes[i].Print();
+					hasFoundId = true;
+					break;
+				}
+			}
+
+			if (!hasFoundId)
+				cout << "Record not found!\n";
+
+			delete[] planes;
+			inFile.close();
+		}
+		outFile.flush();
+	}
+	outFile.close();
+}
